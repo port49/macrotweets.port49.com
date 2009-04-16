3e333e5a3973a92a14fcf5d6c28feb78a84634a6
clr <clr@port49.com>
2009-04-16 17:50:44 -0400

    -- all done now, right? --

--- app/models/macro_tweet.rb
+++ app/models/macro_tweet.rb
@@ -1,2 +1,8 @@
 class MacroTweet < ActiveRecord::Base
+  attr_accessor :sig
+  
+  def validate
+    Errors.add( 'sig', "Nope." ) unless self.sig == "wtf?!?"
+  end
+  
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,43 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+default_run_options[:pty] = true
+set :application, "macrotweets.port49.com"
+set :repository,  "git@github.com:port49/macrotweets.port49.com.git"
+set :scm, :git
+set :ssh_options, { :forward_agent => true }
+
+set :runner, 'macrotweets'
+set :branch, 'macrotweets'
+set :deploy_via, :remote_cache
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/home/macrotweets/public_html"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 # set :scm, :subversion
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+role :app, "macrotweets.port49.com"
+role :web, "macrotweets.port49.com"
+role :db,  "macrotweets.port49.com", :primary => true
+
+namespace :passenger do
+
+  desc 'Restart Application'
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+end
+
+namespace :deploy do
+  
+  desc "Link in the production database.yml" 
+  task :after_update_code do
+    run "ln -nfs #{deploy_to}/#{shared_dir}/system/database.yml #{release_path}/config/database.yml"
+  end
+end
+
+after :deploy, 'passenger:restart'
+after :deploy, 'deploy:cleanup'
+
